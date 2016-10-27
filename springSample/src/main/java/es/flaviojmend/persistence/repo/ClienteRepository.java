0b4dcdcf26cf2f29f23c485671935f07ecec229f
Flávio Mendes <flaviojmendes@gmail.com>
2016-10-27 12:02:59 -0200

    persistindo cliente

---
+++ springSample/src/main/java/es/flaviojmend/persistence/repo/ClienteRepository.java
@@ -0,0 +1,14 @@
+package es.flaviojmend.persistence.repo;
+
+import es.flaviojmend.persistence.entity.Cliente;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by flavio on 19/07/16.
+ */
+public interface ClienteRepository extends CrudRepository<Cliente, Long> {
+
+
+
+}
+
