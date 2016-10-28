4143bd5a76b3a6e7680c2f095d3acdd54b67234d
Adriano Luro <AdrianoLuro@MBP-de-Adriano.nabs.com.br>
2016-10-28 11:47:49 -0200

    cadastroMassagista

--- springSample/src/main/java/es/flaviojmend/controller/MassagistaController.java
+++ springSample/src/main/java/es/flaviojmend/controller/MassagistaController.java
@@ -1,7 +1,9 @@
 package es.flaviojmend.controller;
 
 import es.flaviojmend.persistence.entity.Cliente;
+import es.flaviojmend.persistence.entity.Massagista;
 import es.flaviojmend.service.ClienteService;
+import es.flaviojmend.service.MassagistaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -18,42 +20,16 @@ public class MassagistaController {
 
 
     @Autowired
-    ClienteService clienteService;
-
-    @CrossOrigin
-    @RequestMapping(method = RequestMethod.GET)
-    public Iterable<Cliente> get() {
-        return clienteService.listarClientes();
-    }
+    MassagistaService massagistaService;
 
     @CrossOrigin
     @RequestMapping(method = RequestMethod.POST)
-    public ResponseEntity<?> post(@RequestBody Cliente cliente) throws Exception {
-        clienteService.cadastrarCliente(cliente);
+    public ResponseEntity<?> post(@RequestBody Massagista massagista) throws Exception {
+        massagistaService.cadastrarMassagista(massagista);
 
         HttpHeaders httpHeaders = new HttpHeaders();
 
         return new ResponseEntity<>(null, httpHeaders, HttpStatus.CREATED);
     }
-
-//    @RequestMapping(method = RequestMethod.PUT)
-//    public ResponseEntity<?> put(@RequestBody Comment comment) {
-//        HttpHeaders httpHeaders = new HttpHeaders();
-//
-//        try {
-//            clienteService.updateComment(comment);
-//        } catch (Exception e) {
-//            return new ResponseEntity<>(null, httpHeaders, HttpStatus.BAD_REQUEST);
-//        }
-//
-//        return new ResponseEntity<>(null, httpHeaders, HttpStatus.CREATED);
-//    }
-//
-//    @RequestMapping(method = RequestMethod.DELETE)
-//    public ResponseEntity<?>  delete(@RequestBody Comment comment) {
-//        clienteService.deleteComment(comment);
-//        HttpHeaders httpHeaders = new HttpHeaders();
-//        return new ResponseEntity<>(null, httpHeaders, HttpStatus.OK);
-//    }
 
 }
--- springSample/src/main/java/es/flaviojmend/persistence/entity/Massagista.java
+++ springSample/src/main/java/es/flaviojmend/persistence/entity/Massagista.java
@@ -13,8 +13,45 @@ public class Massagista {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long idMassagista;
 
-    // private String cpf;
+    private String cpf;
 
+    private String sexo;
+
+    private String nome;
+
+    private String email;
+
+    private String senha;
+
+    private String telefone;
+
+    private String especialidade;
+
+    private Integer nota;
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
 
     public Long getIdMassagista() {
         return idMassagista;
@@ -56,20 +93,6 @@ public class Massagista {
         this.nota = nota;
     }
 
-    private String nome;
-
-    // 16 caracteres
-
-    private String email;
-
-    // private String senha;
-
-    //  private String telefone;
-
-    private String especialidade;
-
-    private Integer nota;
-
     public String getSexo() {
         return sexo;
     }
@@ -77,6 +100,4 @@ public class Massagista {
     public void setSexo(String sexo) {
         this.sexo = sexo;
     }
-
-    private String sexo;
 }
